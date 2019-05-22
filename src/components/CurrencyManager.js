18a7ee23f577558643b31061c42c8bfbb78e65c1
Ponda <dp185197@wusdp185197-r95.corp.ncr.com>
2019-05-22 09:28:58 -0400

    Added refresh and updated time

--- src/components/CurrencyManager.js
+++ src/components/CurrencyManager.js
@@ -4,12 +4,14 @@ import Currency from './Currency.js'
 class CurrencyManager extends Component {
   constructor(props) {
     super(props);
+    this.requestedCurrencies = [];
     this.state = {
       currencyCode: '',
       allData: [],
       dataMap: [],
       refreshRequested: false
-    }
+    };
+    this.recentUpdateTime = this.state.allData[0] ? this.state.allData['time']['updated'] : '';
   }
 
   //
@@ -17,10 +19,11 @@ class CurrencyManager extends Component {
   //   console.log(dataFromChild);
   // };
 
-  handleRequest =  async () => {
+  handleRequest =  async (code = this.state.currencyCode) => {
+    console.log(this.requestedCurrencies);
     let mainURL = 'https://api.coindesk.com/v1/bpi/currentprice/';
     let jsonWord = '.json';
-    let endpoint = mainURL + this.state.currencyCode + jsonWord;
+    let endpoint = mainURL + code + jsonWord;
     let currData = {};
     await fetch(endpoint)
         .then(response => response.json())
@@ -29,18 +32,32 @@ class CurrencyManager extends Component {
           }))}
         ).catch(e => console.log(e));
     //this.props.cb(this.state.receivedData);
+    this.recentUpdateTime = this.state.allData[0] ? this.state.allData[0]['time']['updated'] : '';
     console.log(currData);
     console.log('Yeah! I got this')
     await this.setState(prevState => ({
       dataMap: [...prevState.dataMap,
             <tr>
-              <td>{this.state.currencyCode}</td>
-              <td>{currData['bpi'][this.state.currencyCode]['rate']}</td>
+              <td>{code}</td>
+              <td>{currData['bpi'][code]['rate']}</td>
             </tr>
       ]
       //dataMap: this.state.allData.map(conversion => <li>{conversion['bpi'][this.state.currencyCode]['rate']}</li>)
     }));
     console.log(this.state.allData);
+    this.setState({
+      currencyCode: ''
+    })
+  };
+
+  currencyIsPresent = async () => {
+    if (this.requestedCurrencies.some(item => item === this.state.currencyCode)) {
+      console.log('TRUE');
+    } else {
+      console.log('FALSE');
+      await this.requestedCurrencies.push(this.state.currencyCode);
+      await this.handleRequest();
+    }
   };
 
   handleChange = (event) => {
@@ -49,10 +66,22 @@ class CurrencyManager extends Component {
     })
   };
 
-  handleRefresh = () => {
-    this.setState(prevState => ({
-      refreshRequested: !prevState.refreshRequested
-    }));
+  // handleRefresh2 = () => {
+  //   this.setState(prevState => ({
+  //     refreshRequested: !prevState.refreshRequested
+  //   }));
+  // };
+
+  handleRefresh = async () => {
+    await this.setState({
+      dataMap: [],
+      allData: []
+    });
+    await console.log('Set state');
+    for (let i = 0; i < this.requestedCurrencies.length; i++) {
+      await this.handleRequest(this.requestedCurrencies[i]);
+    }
+    await console.log('Handled Requests');
   };
 
   // handleRefresh = async () => {
@@ -67,18 +96,24 @@ class CurrencyManager extends Component {
   // };
 
   render() {
+    console.log('Rendering');
     return (
         <div>
           <input type='text' value={this.state.currencyCode} onChange={this.handleChange}/>
-          <button onClick={this.handleRequest}>Submit</button>
+          <button onClick={this.currencyIsPresent}>Submit</button>
           <button onClick={this.handleRefresh}>Refresh</button>
           <h1>Rendering all the data below</h1>
           <table>
+            <tr>
+              <th>Currency</th>
+              <th>Rate</th>
+            </tr>
             {this.state.dataMap}
           </table>
+          <h4>Updated {this.recentUpdateTime}</h4>
         </div>
     );
   }
 }
-
+// {this.state.allData[0]['time']['updated'] === '' ? '' : this.state.allData[this.state.allData.length - 1]['time']['updated']}
 export default CurrencyManager;
