8050e7d13ed19fb0514e28cf4f51a891ee5328d6
Ponda <dp185197@wusdp185197-r95.corp.ncr.com>
2019-05-23 13:14:37 -0400

    Updates

---
+++ precache-manifest.68d2f4897090d898d14a7d930983f01d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4dd09e020c6d82f014e0fa18b9c2ce",
+    "url": "/BitPrice/index.html"
+  },
+  {
+    "revision": "37a0fdf86212ca74d5a8",
+    "url": "/BitPrice/static/css/2.08c8ee57.chunk.css"
+  },
+  {
+    "revision": "e27200fe9290cc17b867",
+    "url": "/BitPrice/static/css/main.106e39fe.chunk.css"
+  },
+  {
+    "revision": "37a0fdf86212ca74d5a8",
+    "url": "/BitPrice/static/js/2.a3d65317.chunk.js"
+  },
+  {
+    "revision": "e27200fe9290cc17b867",
+    "url": "/BitPrice/static/js/main.aed6e2a3.chunk.js"
+  },
+  {
+    "revision": "658d7e632186d454e933",
+    "url": "/BitPrice/static/js/runtime~main.46858546.js"
+  }
+]);
