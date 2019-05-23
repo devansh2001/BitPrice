1d4877886b5e1793e2edf6d7659522e727463a65
Ponda <dp185197@wusdp185197-r95.corp.ncr.com>
2019-05-23 13:47:36 -0400

    Remove consolelogs

--- src/components/CurrencyManager.js
+++ src/components/CurrencyManager.js
@@ -29,7 +29,7 @@ class CurrencyManager extends Component {
     let change = 0.00000000000000000000000;
     let changeColor = 'black';
     let bgColor = 'white';
-    console.log(this.requestedCurrencies);
+    // console.log(this.requestedCurrencies);
     let mainURL = 'https://api.coindesk.com/v1/bpi/currentprice/';
     let jsonWord = '.json';
     let endpoint = mainURL + code + jsonWord;
@@ -37,16 +37,16 @@ class CurrencyManager extends Component {
     await fetch(endpoint)
         .then(response => response.json())
         .then(data => {currData = data;
-        console.log('OLD: ' + this.oldRates[code]);
+        // console.log('OLD: ' + this.oldRates[code]);
         currencyNewRate = parseFloat(currData['bpi'][code]['rate'].replace(/[,]/, ''));
         this.oldRates[code] = currencyNewRate;
         change = (currencyNewRate - currencyPreviousRate) / currencyPreviousRate;
-        console.log('Rates Below:----');
-        console.log(currencyNewRate - currencyPreviousRate);
-        console.log(currencyPreviousRate);
-        console.log('----');
-        console.log('INF: ' + isFinite(change));
-        console.log('NaN: ' + isNaN(change));
+        // console.log('Rates Below:----');
+        // console.log(currencyNewRate - currencyPreviousRate);
+        // console.log(currencyPreviousRate);
+        // console.log('----');
+        // console.log('INF: ' + isFinite(change));
+        // console.log('NaN: ' + isNaN(change));
         if (currencyPreviousRate == 0) {
           change = 0;
         }
@@ -60,14 +60,14 @@ class CurrencyManager extends Component {
           changeColor = 'black';
           bgColor = 'white'
         }
-        console.log('Changed Rate to ' + currencyNewRate);
+        // console.log('Changed Rate to ' + currencyNewRate);
         this.setState(prevState => ({
             allData: [...prevState.allData, data]
           }))}
         ).catch(e => console.log(e));
     this.recentUpdateTime = this.state.allData[0] ? this.state.allData[0]['time']['updated'] : '';
-    console.log(currData);
-    console.log('Yeah! I got this');
+    // console.log(currData);
+    // console.log('Yeah! I got this');
     change *= 100;
     await this.setState(prevState => ({
       dataMap: [...prevState.dataMap,
@@ -81,33 +81,28 @@ class CurrencyManager extends Component {
             </tr>
       ]
     }));
-    console.log(this.state.allData);
-
-    console.log('Old Rate: ' + currencyPreviousRate);
-    console.log('New Rate: ' + currencyNewRate);
-    console.log('Change Percent: ' + ((currencyNewRate - currencyPreviousRate) / currencyPreviousRate));
-    console.log('Old array: ' + this.oldRates);
+    // console.log(this.state.allData);
+    //
+    // console.log('Old Rate: ' + currencyPreviousRate);
+    // console.log('New Rate: ' + currencyNewRate);
+    // console.log('Change Percent: ' + ((currencyNewRate - currencyPreviousRate) / currencyPreviousRate));
+    // console.log('Old array: ' + this.oldRates);
   };
 
   currencyIsPresent = async (e) => {
     e.preventDefault(e);
     if (this.requestedCurrencies.some(item => item === this.state.currencyCode)) {
-      console.log('TRUE');
+      // console.log('TRUE');
     } else {
-      console.log('FALSE');
+      // console.log('FALSE');
       await this.requestedCurrencies.push(this.state.currencyCode);
       await this.handleRequest();
-      // this.setState({
-      //   currencyCode: ''
-      // });
     }
   };
 
   handleChange = (event) => {
     this.setState({
       currencyCode: event.target.value
-
-      //currencyCode: event.target.value.toUpperCase()
     })
   };
 
@@ -117,17 +112,17 @@ class CurrencyManager extends Component {
       dataMap: [],
       allData: []
     });
-    await console.log('Set state');
+    // await console.log('Set state');
     this.handleRequest('USD');
     for (let i = 0; i < this.requestedCurrencies.length; i++) {
       await this.handleRequest(this.requestedCurrencies[i]);
     }
-    await console.log('Handled Requests');
+    // await console.log('Handled Requests');
   };
 
 
   render() {
-    console.log('Rendering');
+    // console.log('Rendering');
     return (
         <div className='currency-manager'>
           <form className={'input-form'} onSubmit={this.currencyIsPresent}>
