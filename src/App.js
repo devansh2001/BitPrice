460ab436cf975ac2a38299b20509cd90605f76f6
Ponda <dp185197@wusdp185197-r95.corp.ncr.com>
2019-05-23 11:43:28 -0400

    Changes to package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import CurrencyManager from "./components/CurrencyManager";
-import myRequest from "./testrequest";
 import Header from "./components/Header";
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,5 @@
 import React, {Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Image from 'react-bootstrap/Image'
 
 class Header extends Component {
 
